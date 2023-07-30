a18acba81e3358f9690d0ce22f81a7307443e259
manisha24-cell <82254471+manisha24-cell@users.noreply.github.com>
2023-07-30 12:09:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Calculator from './calculator';
+function App() {
+  return (
+    <div className="App">
+    <h1>CALCULATOR</h1>
+    <Calculator/>  
+    </div>
+  );
+}
+
+export default App;
